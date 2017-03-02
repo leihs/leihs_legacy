71c7a95ed6b7710d2a3d3609f6fe09bb333fa6cf
Matúš Kmiť <matus.kmit@zhdk.ch>
2017-03-02 13:31:28 +0100

    Fix user name in statistics nr. 6

--- app/models/statistics/base.rb
+++ app/models/statistics/base.rb
@@ -2,30 +2,6 @@ module Statistics
   class Base
     class << self
 
-      # Who borrowed the most things?
-      # def most_borrower(date_range = nil)
-      #   #Contract.select("user_id, count(*) as c")
-      #     .group_by(&:user_id).order_by("c desc").first
-      #   Contract.group_by(&:user_id).first.first
-      # end
-
-      # Which inventory pool is busiest?
-      # def busiest_inventory_pool
-      #   Contract.group_by(&:inventory_pool_id).first.first
-      # end
-
-      # Who bought the most items?
-      # def most_items_inventory_pool
-      #   Item.all.group_by(&:inventory_pool_id).first.first
-      # end
-
-      # Which inventory pool costs the most money?
-      # def most_money_inventory_pool
-      #   nil
-      # end
-
-      #################################################
-
       def hand_overs(klasses, options = {})
         options.symbolize_keys!
         options[:limit] ||= 10
@@ -134,11 +110,6 @@ module Statistics
           .limit(options[:limit])
 
         query = case klass.name
-                  # when "User"
-                  #  query.joins(:reservations).
-                  #    group("contracts.#{klass.name.foreign_key}").
-                  #    select("CAST(CONCAT_WS(' ', users.firstname,
-                  #    users.lastname) AS CHAR) AS label")
                 when 'InventoryPool'
                   query.joins(:own_items)
                     .group('items.owner_id')
@@ -153,8 +124,6 @@ module Statistics
                   raise "#{klass} not supported"
                 end
 
-        # query = query.where(:contracts => {:user_id => options[:user_id]})
-        # if options[:user_id]
         unless options[:inventory_pool_id].blank?
           query = query.where(items: { owner_id: options[:inventory_pool_id] })
         end
@@ -188,9 +157,11 @@ module Statistics
                   query.joins(reservations: :contract)
                     .group("reservations.#{klass.name.foreign_key}")
                     .group('users.id')
-                    .select("CAST(CONCAT_WS(' ', " \
-                            'users.firstname, ' \
-                            'users.lastname) AS CHAR) AS label')
+                    .select(<<~SQL)
+                      CONCAT_WS(' ',
+                                users.firstname,
+                                users.lastname) AS label
+                    SQL
                 when 'InventoryPool'
                   query.joins(reservations: :contract)
                     .group('inventory_pools.id')
