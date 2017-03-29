2dbecc39bd5b42e519ab435aabdf13826b5e1551
Matúš Kmiť <matus.kmit@zhdk.ch>
2017-03-29 11:55:27 +0200

    Extend admin settings

--- engines/leihs_admin/spec/steps/settings_steps.rb
+++ engines/leihs_admin/spec/steps/settings_steps.rb
@@ -13,51 +13,61 @@ module LeihsAdmin
         @new_settings = {}
         within("form#edit_setting[action='/admin/settings']") do
           table.raw.flatten.each do |k|
-            case k
-            when \
-              'email_signature',
-              'mail_delivery_method',
-              'smtp_address',
-              'smtp_domain',
-              'smtp_password',
-              'smtp_username',
-              'user_image_url',
-              'smtp_openssl_verify_mode'
-              field = find("input[name='setting[#{k}]']")
-              expect(Setting.send(k).to_s).to eq field.value
-              @new_settings[k] = new_value = Faker::Lorem.word
-              field.set new_value
-            when 'logo_url'
-              field = find("input[name='setting[#{k}]']")
-              expect(Setting.send(k).to_s).to eq field.value
-              @new_settings[k] = field.value
-            when 'default_email'
-              field = find("input[name='setting[#{k}]']")
-              expect(Setting.send(k).to_s).to eq field.value
-              @new_settings[k] = new_value = Faker::Internet.email
-              field.set new_value
-            when 'contract_lending_party_string', 'contract_terms'
-              field = find("textarea[name='setting[#{k}]']")
-              expect(Setting.send(k).to_s).to eq field.value
-              @new_settings[k] = new_value = Faker::Lorem.paragraph
-              field.set new_value
-            when 'deliver_order_notifications', 'smtp_enable_starttls_auto'
-              field = find("input[name='setting[#{k}]']")
-              expect(Setting.send(k)).to eq field.checked?
-              # TODO: @new_settings[k]
-              field.click
-            when 'smtp_port'
-              field = find("input[name='setting[#{k}]']")
-              expect(Setting.send(k).to_s).to eq field.value
-              @new_settings[k] = new_value = rand(0..10000)
-              field.set new_value
-            when 'time_zone', 'local_currency_string'
-              field = find("select[name='setting[#{k}]']")
-              expect(Setting.send(k).to_s).to eq field.value
-              @new_settings[k] = new_value = field.all('option').sample[:value]
-              field.select new_value
-            else
-              raise format('%s not found', k)
+            begin
+              case k
+              when \
+                'email_signature',
+                'external_base_url',
+                'ldap_config',
+                'mail_delivery_method',
+                'smtp_address',
+                'smtp_domain',
+                'smtp_openssl_verify_mode',
+                'smtp_password',
+                'smtp_username',
+                'user_image_url'
+                field = find("input[name='setting[#{k}]']")
+                expect(Setting.send(k).to_s).to eq field.value
+                @new_settings[k] = new_value = Faker::Lorem.word
+                field.set new_value
+              when 'logo_url'
+                field = find("input[name='setting[#{k}]']")
+                expect(Setting.send(k).to_s).to eq field.value
+                @new_settings[k] = field.value
+              when 'default_email'
+                field = find("input[name='setting[#{k}]']")
+                expect(Setting.send(k).to_s).to eq field.value
+                @new_settings[k] = new_value = Faker::Internet.email
+                field.set new_value
+              when \
+                'contract_lending_party_string',
+                'contract_terms',
+                'custom_head_tag'
+                field = find("textarea[name='setting[#{k}]']")
+                expect(Setting.send(k).to_s).to eq field.value
+                @new_settings[k] = new_value = Faker::Lorem.paragraph
+                field.set new_value
+              when 'deliver_order_notifications', 'smtp_enable_starttls_auto'
+                field = find("input[name='setting[#{k}]']")
+                expect(Setting.send(k)).to eq field.checked?
+                # TODO: @new_settings[k]
+                field.click
+              when 'smtp_port'
+                field = find("input[name='setting[#{k}]']")
+                expect(Setting.send(k).to_s).to eq field.value
+                @new_settings[k] = new_value = rand(0..10000)
+                field.set new_value
+              when 'time_zone', 'local_currency_string'
+                field = find("select[name='setting[#{k}]']")
+                expect(Setting.send(k).to_s).to eq field.value
+                @new_settings[k] = new_value = field.all('option').sample[:value]
+                field.select new_value
+              else
+                raise format('%s not found', k)
+              end
+            rescue Selenium::WebDriver::Error::UnknownError
+              page.execute_script %[ $(".navbar").remove() ]
+              retry
             end
           end
         end
