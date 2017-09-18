4f8ae15319d141f9050591dda14015ed0a1bde5f
Max Albrecht <max.albrecht@zhdk.ch>
2017-09-18 12:57:13 +0200

    fix: HAML whitespace/indent dev === prod

--- config/initializers/haml.rb
+++ config/initializers/haml.rb
@@ -1,2 +1,5 @@
 # NOTE we cannot hyphenated data attributes (yet), bacause autocomplete.coffee relies on underscored data attributes
 Haml::Template.options[:hyphenate_data_attrs] = false
+
+# force same whitespace behaviour in dev as in prod (!!!)
+Haml::Template.options[:ugly] = true
