86bfb1d1651f5c7dd06c6dc6fda341dd6f34b6f2
phiku <philipp.kuederli@zhdk.ch>
2018-03-27 15:33:26 +0200

    fix: sort pools alphabetically for borrow

--- app/controllers/borrow/inventory_pools_controller.rb
+++ app/controllers/borrow/inventory_pools_controller.rb
@@ -1,6 +1,6 @@
 class Borrow::InventoryPoolsController < Borrow::ApplicationController
 
   def index
-    @inventory_pools = current_user.inventory_pools
+    @inventory_pools = current_user.inventory_pools.reorder(:name)
   end
 end
