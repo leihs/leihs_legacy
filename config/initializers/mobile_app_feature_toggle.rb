d3c4bcada820d8681c590380d812637fda7ad13f
Max Albrecht <max.albrecht@zhdk.ch>
2022-04-21 16:57:50 +0200

    new borrow: feature toggles, notice banner, texts

--- config/initializers/mobile_app_feature_toggle.rb
+++ config/initializers/mobile_app_feature_toggle.rb
@@ -1,2 +1,28 @@
-# NOTE: set to `true` on deploy if mobile app is installed alongside
-LEIHS_MOBILE_APP_ENABLED = false
+# NOTE: values will be filled out when deploying, those are just for development
+# LEIHS_MOBILE_APP_ENABLED = false
+LEIHS_MOBILE_APP_ENABLED = true
+LEIHS_NEW_BORROW_APP_DEFAULT_TO_MOBILE = false
+LEIHS_MOBILE_APP_SHOW_NOTICE = true
+
+LEIHS_MOBILE_APP_SHOW_NOTICE_TEXTS = {}
+
+LEIHS_MOBILE_APP_SHOW_NOTICE_TEXTS["de-CH"] = <<~TXT.strip
+Nutze die neue <b style="text-decoration: underline">Mobile-App</b> mit neuen Funktionen
+TXT
+
+# LEIHS_MOBILE_APP_SHOW_NOTICE_TEXTS["gsw-CH"] = <<~TXT.strip
+#   Nutze die neue <b style="text-decoration: underline">Mobile-App</b> mit neuen Funktionen
+# TXT
+
+LEIHS_MOBILE_APP_SHOW_NOTICE_TEXTS["en-GB"] = <<~TXT.strip
+  Use the new <b style="text-decoration: underline">mobile app</b> with new features
+TXT
+
+LEIHS_MOBILE_APP_SHOW_NOTICE_TEXTS["fr-CH"] = <<~TXT.strip
+  Utilise la nouvelle <b style="text-decoration: underline">application mobile</b> avec de nouvelles fonctions
+TXT
+
+LEIHS_MOBILE_APP_SHOW_NOTICE_TEXTS["es"] = <<~TXT.strip
+  Utilice la nueva <b style="text-decoration: underline">aplicación móvil</b> con nuevas funciones
+TXT
+
