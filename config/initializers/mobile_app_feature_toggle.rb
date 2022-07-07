0d16c9f6f7d8c77b0cf38dacfb9af99d319190cd
uvb <urs.vanbinsbergen@zhdk.ch>
2022-07-07 15:29:09 +0200

    reword and redesign borrow app banner

--- config/initializers/mobile_app_feature_toggle.rb
+++ config/initializers/mobile_app_feature_toggle.rb
@@ -7,22 +7,26 @@ LEIHS_MOBILE_APP_SHOW_NOTICE = true
 LEIHS_MOBILE_APP_SHOW_NOTICE_TEXTS = {}
 
 LEIHS_MOBILE_APP_SHOW_NOTICE_TEXTS["de-CH"] = <<~TXT.strip
-Nutze die neue <b style="text-decoration: underline">Mobile-App</b> mit neuen Funktionen
+  <span class="mobile-app-notice-desktop">Wechsle zur <b style="text-decoration: underline;">neuen Oberfläche</b> mit neuen Funktionen</span>
+  <span class="mobile-app-notice-mobile">Wechsle zu <b style="text-decoration: underline;">Leihs Mobile</b></span>
 TXT
 
 # LEIHS_MOBILE_APP_SHOW_NOTICE_TEXTS["gsw-CH"] = <<~TXT.strip
-#   Nutze die neue <b style="text-decoration: underline">Mobile-App</b> mit neuen Funktionen
+#   <span class="mobile-app-notice-desktop">Wechsle zur <b style="text-decoration: underline;">neuen Oberfläche</b> mit neuen Funktionen</span>
+#   <span class="mobile-app-notice-mobile">Wechsle zu <b style="text-decoration: underline;">Leihs Mobile</b></span>
 # TXT
 
 LEIHS_MOBILE_APP_SHOW_NOTICE_TEXTS["en-GB"] = <<~TXT.strip
-  Use the new <b style="text-decoration: underline">mobile app</b> with new features
+  <span class="mobile-app-notice-desktop">Switch to the <b style="text-decoration: underline;">new user interface</b> with new features</span>
+  <span class="mobile-app-notice-mobile">Switch to <b style="text-decoration: underline;">Leihs Mobile</b></span>
 TXT
 
 LEIHS_MOBILE_APP_SHOW_NOTICE_TEXTS["fr-CH"] = <<~TXT.strip
-  Utilise la nouvelle <b style="text-decoration: underline">application mobile</b> avec de nouvelles fonctions
+  <span class="mobile-app-notice-desktop">Passe à la <b style="text-decoration: underline;">nouvelle interface</b> avec de nouvelles fonctions</span>
+  <span class="mobile-app-notice-mobile">Passe à <b style="text-decoration: underline;">Leihs Mobile</b></span>
 TXT
 
 LEIHS_MOBILE_APP_SHOW_NOTICE_TEXTS["es"] = <<~TXT.strip
-  Utilice la nueva <b style="text-decoration: underline">aplicación móvil</b> con nuevas funciones
+  <span class="mobile-app-notice-desktop">Cambiar a la <b style="text-decoration: underline;">nueva interfaz</b> con nuevas funciones</span>
+  <span class="mobile-app-notice-mobile">Cambiar a <b style="text-decoration: underline;">Leihs Mobile</b></span>
 TXT
-
