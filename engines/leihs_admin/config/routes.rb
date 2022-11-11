d10d51f8b7dec88d6dce062c9198db5aec18a1b1
Matus Kmit <matuskmit1@gmail.com>
2022-11-11 16:23:06 +0100

    remove fields from admin engine

--- engines/leihs_admin/config/routes.rb
+++ engines/leihs_admin/config/routes.rb
@@ -6,22 +6,8 @@ LeihsAdmin::Engine.routes.draw do
     get 'top', to: 'admin#top'
   end
 
-  resources :users,           only: :index
-
   # Audits
   get 'audits',           to: 'audits#index'
   get ':type/:id/audits', to: 'audits#index', as: 'individual_audits'
 
-  # Fields
-  get 'fields', to: 'fields#index'
-  post 'batch_update_fields', to: 'fields#batch_update'
-
-  get 'fields_editor', to: 'fields_editor#edit_react'
-  delete 'fields_editor/:id', to: 'fields_editor#destroy'
-  get 'fields_editor/all_fields', to: 'fields_editor#all_fields'
-  get 'fields_editor/groups', to: 'fields_editor#groups'
-  get 'fields_editor/single_field/(:id)', to: 'fields_editor#single_field', :as => 'fields_editor_single_field'
-  get 'fields_editor/edit_react', to: 'fields_editor#edit_react'
-  put 'fields_editor/new_react', to: 'fields_editor#new_react'
-  post 'fields_editor/update_react', to: 'fields_editor#update_react'
 end
