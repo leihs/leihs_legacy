622b2b734c4e4de2e3e52d433bcbb17a44a401ee
Matus Kmit <matuskmit1@gmail.com>
2023-03-02 15:45:11 +0530

    delete old unusable cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_leihs_session'
+Rails.application.config.session_store :cookie_store, key: Leihs::Constants::Legacy::SESSION_NAME
---
+++ lib/leihs/constants/legacy.rb
@@ -0,0 +1,7 @@
+module Leihs
+  module Constants
+    module Legacy
+      SESSION_NAME = '_leihs_session'
+    end
+  end
+end
