e80119884be4865eba97242623d73d6ccdf6a13a
Matus Kmit <matuskmit1@gmail.com>
2024-07-12 13:34:46 +0200

    chore: update gems

--- factories/building_factory.rb
+++ factories/building_factory.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
 
   factory :building do
-    name { Faker::Lorem.words(number: 3).join(" ").capitalize }
+    name { Faker::Lorem.words(number: 4).join(" ").capitalize }
     sequence(:code) do
       c = name.split(" ").map(&:first).join.upcase 
       "#{c}#{_1}"
--- factories/room_factory.rb
+++ factories/room_factory.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
 
   factory :room do
-    name { Faker::Lorem.words(number: 3).join.capitalize }
+    name { Faker::Lorem.words(number: 4).join(" ").capitalize }
     building
   end
 
